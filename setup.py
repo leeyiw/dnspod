584f893ed79481983df44a192b0ee9be9d867830
leeyiw <leeyiw@gmail.com>
2014-12-23 16:38:35 +0800

    add setup scripts

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='dnspod',
+    version='0.1',
+    description='a Python library for DNSPod OpenAPI',
+    author='Yiwei Li',
+    author_email='leeyiw@gmail.com',
+    url='https://github.com/leeyiw/dnspod',
+    packages=['dnspod'],
+)
